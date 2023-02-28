f428eceedc62036df4e978be45d7e985d5e8abdb
Sandip Nandy <sandeepnandi4@gmail.com>
2023-02-28 13:55:00 +0530

    Email sending process update

--- app/Mail/EmailVerificationOtp.php
+++ app/Mail/EmailVerificationOtp.php
@@ -5,8 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class EmailVerificationOtp extends Mailable
@@ -15,50 +13,14 @@ class EmailVerificationOtp extends Mailable
 
     public $mailDetails;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
     public function __construct($mailDetails)
     {
         $this->mailDetails = $mailDetails;
     }
 
-    /**
-     * Get the message envelope.
-     *
-     * @return \Illuminate\Mail\Mailables\Envelope
-     */
-    public function envelope()
+    public function build()
     {
-        return new Envelope(
-            subject: config('app.name') . ' - Verify your Email',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
-    public function content()
-    {
-        return new Content(
-            markdown: 'emails.email-verification-otp',
-            with: [
-                'mailDetails' => $this->mailDetails,
-            ]
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array
-     */
-    public function attachments()
-    {
-        return [];
+        $subject = config('app.name') . ' - Verify your Email';
+        return $this->subject($subject)->markdown('emails.email-verification-otp')->with('mailDetails', $this->mailDetails );
     }
 }
