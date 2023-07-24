8449218caca398beae83057c0872801273033870
Sandip Nandy <sandeepnandi4@gmail.com>
2023-07-24 23:35:44 +0530

    Chat message notification

--- app/Http/Controllers/Api/ChatMessageController.php
+++ app/Http/Controllers/Api/ChatMessageController.php
@@ -5,46 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Api\Controller;
 use Illuminate\Http\Request;
 
-use App\Events\NewMessageSent;
 use App\Http\Requests\GetMessageRequest;
 use App\Http\Requests\StoreMessageRequest;
 use App\Models\Chat;
 use App\Models\ChatMessage;
+use App\Models\ChatParticipant;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Auth;
 use Validator;
+use App\Notifications\ChatMessageNotification;
 
 class ChatMessageController extends Controller
 {
     public function __construct(){
         $this->middleware('auth:api');
         $this->user = auth('api')->user();
-    }
-    /**
-     * Gets chat message
-     *
-     * @param GetMessageRequest $request
-     * @return JsonResponse
-     */
-    public function index(Request $request): JsonResponse
-    {
-        $roomId = $request->room_id ?? null;
-        $currentPage = $request->page ?? 1;
-        $pageSize = $request->page_size ?? 15;
-        echo $roomId;
-        $messages = ChatMessage::where('chat_id', $roomId)
-                                //->with('user')
-                                ->latest('created_at')
-                                ->get()
-                                /* ->simplePaginate(
-                                    $pageSize,
-                                    ['*'],
-                                    'page',
-                                    $currentPage
-                                ) */;
-        print_r($messages);
-        return $this->success($messages);
     }
 
     /**
@@ -66,6 +42,11 @@ class ChatMessageController extends Controller
             return response()->json($validator->errors()->toJson(), 422);
         }
 
+        $checkUser = ChatParticipant::where('chat_id', $request->chat_id)->where('user_id', $this->user->id)->exists();
+        if( !$checkUser ){
+            return response()->json(['success' => false, 'message' => 'You are not allowed to send message to this chat!'], 400);
+        }
+
         $data['chat_id'] = $request->chat_id;
         $data['user_id'] = $this->user->id;
         $data['message'] = $request->message;
@@ -73,45 +54,26 @@ class ChatMessageController extends Controller
         $chatMessage = ChatMessage::create($data);
         $chatMessage->load('user');
 
-        /// TODO send broadcast event to pusher and send notification to onesignal services
-        $this->sendNotificationToOther($chatMessage);
+        //+++++++++++++++++++++++ CHAT NOTIFICATION :: Start +++++++++++++++++++++++//
+        $otherUsers = ChatParticipant::where('chat_id', $request->chat_id)->where('user_id', '!=', $this->user->id)->get();
+
+        if( !empty($otherUsers) ){
+            foreach($otherUsers as $otherUserVal){
+                $otherUserVal->user_id;
+                $chatUser = User::whereId($otherUserVal->user_id)->where('status', 1)->first();
+
+                if($chatUser){
+                    $notoficationParams = [];
+                    $notoficationParams['message_by_user_id'] = $this->user->id;
+                    $notoficationParams['message_by_user_uuid'] = $this->user->uuid ?? null;
+
+                    $chatUser->notify(new ChatMessageNotification($notoficationParams));
+                }
+            }
+        }
+        //+++++++++++++++++++++++ CHAT NOTIFICATION :: End +++++++++++++++++++++++//
 
         return $this->success('Message has been sent successfully.');
     }
-
-    /**
-     * Send notification to other users
-     *
-     * @param ChatMessage $chatMessage
-     */
-    private function sendNotificationToOther(ChatMessage $chatMessage) : void {
-
-        // TODO move this event broadcast to observer
-        broadcast(new NewMessageSent($chatMessage))->toOthers();
-
-        $user = $this->user;
-        $userId = $user->id;
-
-        $chat = Chat::where('id',$chatMessage->chat_id)
-            ->with(['participants'=>function($query) use ($userId){
-                $query->where('user_id','!=',$userId);
-            }])
-            ->first();
-        if(count($chat->participants) > 0){
-            $otherUserId = $chat->participants[0]->id;
-
-            $otherUser = User::where('id',$otherUserId)->first();
-            $otherUser->sendNewMessageNotification([
-                'messageData'=>[
-                    'senderName'=>$user->username,
-                    'message'=>$chatMessage->message,
-                    'chatId'=>$chatMessage->chat_id
-                ]
-            ]);
-
-        }
-
-    }
-
 
 }
